667678fa03d0c533a8f5125c4a96016c06c01420
pierreg256 <pierre.gilot@me.com>
2016-03-07 15:36:01 +0100

    Initial Commit

---
+++ TVPartyCore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TVPartyCore
+//
+//  Created by Gilot, Pierre on 07/03/2016.
+//  Copyright © 2016 Gilot, Pierre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
